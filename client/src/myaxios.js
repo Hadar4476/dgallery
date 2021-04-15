f1304a5ea18f620a06174835dede8cbfcb30197c
Hadar4476 <hadar4476@gmail.com>
2021-04-15 10:09:28 +0300

    added padding to FormGroupWrapper css class at all forms

--- client/src/myaxios.js
+++ client/src/myaxios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const awsS3URL = 'https://dgallery-bucket.s3.us-east-2.amazonaws.com/';
-// const url = 'https://dgallery.herokuapp.com';
-const url = 'http://localhost:3005';
+const url = 'https://dgallery.herokuapp.com';
 
 const instance = axios.create({
   baseURL: url,
