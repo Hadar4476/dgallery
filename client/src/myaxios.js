4bb8282cc1ddab360c166f2c8f0604bd3e388971
Hadar4476 <hadar4476@gmail.com>
2021-05-02 10:26:13 +0300

    at myaxios.js, changed url

--- client/src/myaxios.js
+++ client/src/myaxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const awsS3URL = 'https://dgallery-bucket.s3.us-east-2.amazonaws.com/';
-const url = 'https://dgallery.herokuapp.com';
+const url = 'https://dgallery-server.herokuapp.com';
 
 const instance = axios.create({
   baseURL: url,
